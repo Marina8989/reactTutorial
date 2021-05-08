271b742f44d1c389340998784f377ccbff1fa1b5
Marina8989 <marina.mill209@gmail.com>
2021-05-07 17:49:41 -0700

    react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Product from './Product'
 import products from './vProducts'
 
 
-function App() {
-    const productItems = products.map(item => <Product key={item.id} name={item.name} price={item.price} description={item.description}/>)
 
-  return (
-       <div>
-           { productItems }
-       </div>
-  );
-}
 
-export default App;
+
--- src/SearchButton.js
+++ src/SearchButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SearchButton() {
     return (
-        <div style={{color: 'red', backgroundColor: 'green', padding: '5px', width: '47px'}}>
+        <div style={{color: 'red', backgroundColor: 'green', padding: '5px', width: '47tpx'}}>
             <button className="button">Click</button>
         </div>
     )
