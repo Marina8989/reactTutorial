a500d1602bdb7b2e7780200d14696ecc7980f16e
Marina8989 <marina.mill209@gmail.com>
2021-05-07 18:18:07 -0700

    react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
-import Product from './Product'
-import products from './vProducts'
+//import Product from './Product'
+//import products from './vProducts'
+
+class App extends Component{
+  constructor() {
+    super()
+      this.state = {
+        name: 'Marina',
+        age: 31
+      }
+  }
+  render() {
+    return(
+      <div>
+          <h1>{this.state.name}</h1>
+          <p>{this.state.age}</p>
+      </div>
+    )
+  }
+}
 
 
-
+export default App
 
 
--- src/index.js
+++ src/index.js
@@ -1,48 +1,9 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 
 
-class App extends React.Component {
-     render() {
-       return(
-         <div>
-           <Header username="Marina"/>
-           <Greeting />
-         </div>
-       )
-     }
-}
 
-class Header extends React.Component{
-    render() {
-        return(
-            <header>
-                <h1>Welcome, {this.props.username}! How are you today?</h1>
-            </header>
-        )
-    }
-}
-
-class Greeting extends Component{
-    render() {
-        let data = new Date();
-        let hours = data.getHours();
-        let timeOfDay;
-
-        if(hours < 12) {
-          timeOfDay = 'morning';
-        }else if(hours >= 12 && hours < 17) {
-          timeOfDay = 'afternoon';
-        }else {
-            timeOfDay = 'night';
-        }
-
-        return(
-             <h1>Good {timeOfDay}, to you! </h1>
-        )
-    }
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
